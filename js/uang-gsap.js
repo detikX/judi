db55e482888939f6f1905a641bd4b035ef9e032c
dediwibisono17 <dedi.wibisono17@gmail.com>
2024-05-24 13:04:23 +0700

    update

---
+++ js/uang-gsap.js
@@ -0,0 +1,35 @@
+gsap.registerPlugin(ScrollTrigger);
+
+
+var targetsHeight = document.querySelectorAll(".height");
+
+
+
+
+
+targetsHeight.forEach((target, index) => {
+    const pinSpacing = index === targetsHeight.length - 1 ? "true" : false;
+    const end =
+        index === targetsHeight.length - 1
+            ? `+=${target.offsetHeight}px`
+            : `+=${targetsHeight[index + 1].offsetHeight}px`;
+    const opacityFirst = index === 0 ? 1 : 0;
+    const opacitySecond = index === targetsHeight.length - 1 ? 1 : 0;
+    const tl = gsap
+        .timeline({
+            defaults: { duration: 1 },
+            scrollTrigger: {
+                trigger: target,
+                pin: true,
+                scrub: true,
+                start: "center center",
+                end,
+                markers: true,
+                toggleActions: "restart none reverse reset",
+                pinSpacing,
+            },
+        })
+        .from(target, { opacity: opacityFirst, duration: 0.2 }, 0)
+        .to(target, { opacity: opacitySecond, duration: 0.2 }, 0.8);
+});
+
