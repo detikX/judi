e3442130efc039f5c909f88558caba1ab433cdb6
dediwibisono17 <dedi.wibisono17@gmail.com>
2024-05-28 14:38:34 +0700

    update

--- js/app.js
+++ js/app.js
@@ -1,15 +1,24 @@
-$(window).scroll(function () {
-    let $window = $(window),
-        $body = $('body'),
-        $section = $('.section');
-    let scroll = $window.scrollTop() + ($window.height() / 3);
-    $section.each(function () {
-        if ($(this).position().top <= scroll && $(this).position().top + $(this).height() > scroll) {
-            $body.removeClass(function (index, css) {
-                return (css.match(/(^|\s)color-\s+/g) || [].join(' '));
-            });
-            $('.section').removeClass('active');
-            $(this).addClass('active');
-        }
-    });
-}).scroll();
+window.onscroll = function () { ScrollIndicator() };
+
+// var mobil = "<div class='imgs'><img src='img/mobil-kanan.png' /></div>"
+function ScrollIndicator() {
+    // console.log(document.documentElement.scrollHeight);
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    // var height = $(".ref").height();
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById('footer__progress_bar').style.width = scrolled + "%";
+    // document.getElementById('footer__progress_bar').innerHTML = Math.round(scrolled) + "%"
+    console.log(scrolled);
+    // document.getElementById('footer__progress_bar').innerHTML = mobil;
+
+}
+
+// function myFunction() {
+//     const winScroll = document.getElementById("info").scrollTop;
+//     const height = document.getElementById("info").scrollHeight - document.documentElement.clientHeight;
+//     const scrolled = (winScroll / height) * 100;
+//     document.getElementById("footer__progress_bar").style.width = scrolled + "%";
+// }
+
+// document.getElementById("info").onscroll = function () { myFunction() };
